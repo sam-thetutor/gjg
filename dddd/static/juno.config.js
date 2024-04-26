dd10e939d83b0af65e5c893966632669cd6ff89b
sam-thetutor <atwebembeiresamuel2020@gmail.com>
2024-04-26 21:45:31 +0300

    d

---
+++ dddd/static/juno.config.js
@@ -0,0 +1,9 @@
+import {defineConfig} from '@junobuild/config';
+
+/** @type {import('@junobuild/config').JunoConfig} */
+export default defineConfig({
+  satellite: {
+    id: 'xnjuk-iqaaa-aaaal-ajarq-cai',
+    source: 'public'
+  }
+});
